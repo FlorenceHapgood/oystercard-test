d1119a9eef3f983ccf1204daebb8552a27f82cea
Alex Kharouk <akharoukpp@gmail.com>
2018-09-10 18:06:18 +0100

    add error for touch_in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,35 @@
 class Oystercard
-    attr_reader :balance
+    attr_reader :balance, :journey
 
     def initialize(balance = 0)
         @balance = balance
         @CAPACITY = 90
+        @MINIMUM = 1
+        @journey = false 
     end
 
     def top_up(balance)
         raise "You cannot top up over £#{@CAPACITY}" if @balance >= @CAPACITY
         @balance += balance
     end
+
+    def deduct(fare)
+        @balance -= fare
+    end
+
+    def in_journey?
+        @journey
+    end
+
+    def touch_in
+        raise "You're not passing with THAT balance" if @balance <= @MINIMUM
+        @journey = true
+    end
+
+    def touch_out
+        @journey = false
+    end
 end
+
+# card = Oystercard.new -> card.top_up(10) -> card.in_journey? #false
+# -> card.touch_in -> card.in_journey?
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,20 +10,52 @@ describe Oystercard do
         it "has a default balance of 0 unless stated otherwise" do
             expect(subject.balance).to eq 0
         end
-        
+
     end
 
-    it "allows the user to top up money" do
-        expect(subject.top_up(10)).to eq 10
-    end
+    context "spending and topping up our card" do
+        it "allows the user to top up money" do
+            expect(subject.top_up(10)).to eq 10
+        end
 
-    it "after top up, balance is updated" do
-        subject.top_up(15)
-        expect(subject.balance).to eq 15
-    end
+        it "after top up, balance is updated" do
+            subject.top_up(15)
+            expect(subject.balance).to eq 15
+        end
     
-    let(:card) { described_class.new(90) }
-    it "raises error when balance is more than 90" do
-        expect {card.top_up(1)}.to raise_error("You cannot top up over £90")
+        let(:card) { described_class.new(90) }
+
+        it "raises error when balance is more than 90" do
+            expect {card.top_up(1)}.to raise_error("You cannot top up over £90")
+        end
+
+        it "deducts money from my card balance" do
+            expect(card.deduct(23)).to eq 67
+        end
+    end
+
+    context "touching in and out, and checking if it's in journey" do
+        let(:card) { described_class.new }
+
+        
+        it "determines if card is on a journey" do
+            allow(card).to receive(:in_journey?) { false }
+            expect(card.in_journey?).to eq false 
+        end
+
+        it "touches in, turning journey to true" do
+            card = described_class.new(10)
+            allow(card).to receive(:in_journey?) { false }
+            expect(card.touch_in).to be_truthy
+        end
+        
+        it "touches out, turning journey to false" do
+            allow(card).to receive(:in_journey?) { true }
+            expect(card.touch_out).to be_falsey
+        end
+
+        it "raises error if touching in with balance of less than 1" do
+            expect {card.touch_in}.to raise_error "You're not passing with THAT balance"
+        end
     end
 end
