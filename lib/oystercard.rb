7343ed32a7959ba04c6027e653c974061e54f1c8
Florence Hapgood <flohapgood@icloud.com>
2018-09-11 16:20:09 +0100

    Saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,18 @@
 class Oystercard
-    attr_reader :balance, :journey
+    attr_reader :balance, :journey, :entry_station
+
 
     def initialize(balance = 0)
         @balance = balance
         @CAPACITY = 90
         @MINIMUM = 1
-        @journey = false 
+        @journey = false
+        @entry_station = []
     end
 
-    def top_up(balance)
+    def top_up(amount)
         raise "You cannot top up over £#{@CAPACITY}" if @balance >= @CAPACITY
-        @balance += balance
+        @balance += amount
     end
 
     def deduct(fare)
@@ -18,17 +20,22 @@ class Oystercard
     end
 
     def in_journey?
+      if @entry_station == nil
         @journey
+      end
     end
 
-    def touch_in
+    def touch_in(tube_station)
         raise "You're not passing with THAT balance" if @balance <= @MINIMUM
+        @entry_station << tube_station
         @journey = true
     end
 
     def touch_out
         @journey = false
+        @entry_station = nil
     end
+
 end
 
 # card = Oystercard.new -> card.top_up(10) -> card.in_journey? #false
